35f76e033fa2a97068b8beddb485ef3314b78b24
Anish K <anik2@publicisgroupe.net>
2023-03-26 17:31:41 +0530

    updated

--- demo/src/main/java/com/sample/demo/Dao/PersonRepository.java
+++ demo/src/main/java/com/sample/demo/Dao/PersonRepository.java
@@ -3,7 +3,5 @@ package com.sample.demo.Dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PersonRepository extends JpaRepository {
-    
 
-    
 }
--- demo/src/main/java/com/sample/demo/controller/PersonController.java
+++ demo/src/main/java/com/sample/demo/controller/PersonController.java
@@ -1,5 +1,32 @@
 package com.sample.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sample.demo.Dao.PersonRepository;
+import com.sample.demo.model.Person;
+
+@RestController
 public class PersonController {
     
+    @Autowired
+    PersonRepository repo;
+
+    @PostMapping("/savePerson")
+    public String savePerson(@RequestBody Person person){
+        repo.save(person);
+        return "updated sucess";
+    }
+
+    @GetMapping("/viewPerson")
+    public List<String> getPerson(){
+        return repo.findAll();
+    }
+
+
 }
