26af2ea3c3f457ee773e0dfc1554b034bd6f1d81
Anish K <anik2@publicisgroupe.net>
2023-03-26 19:53:30 +0530

    changes

--- demo/src/main/java/com/sample/demo/controller/PersonController.java
+++ demo/src/main/java/com/sample/demo/controller/PersonController.java
@@ -1,9 +1,11 @@
 package com.sample.demo.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,13 +22,19 @@ public class PersonController {
     @PostMapping("/savePerson")
     public String savePerson(@RequestBody Person person){
         repo.save(person);
-        return "updated sucess";
+        return "created sucess";
     }
 
     @GetMapping("/viewPerson")
     public List<Person> getPerson(){
         return repo.findAll();
     }
+
+    @GetMapping("/viewPersonById{id}")
+    public Optional<Person> getPersonById(@PathVariable Integer id){
+        return repo.findById(id);
+    }
+
 
 
 }
